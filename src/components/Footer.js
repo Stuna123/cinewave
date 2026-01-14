2dce863a3259f952530d10d70325bfb50dc989bb
francis-tabora <ftabora11@gmail.com>
2026-01-14 11:41:04 +0100

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,18 @@ import { Link, NavLink } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer className="bg-white dark:bg-gray-900 text-gray-400">
+    <footer className="bg-white border-b-2 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900 dark:border-b-1 dark:border-gray-700">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="font-medium text-sm text-gray-500">
+            © {new Date().getFullYear()} {" "}
+            <Link to="/" className="font-bold text-black dark:text-white">CineWave</Link>. 
+            All Rights Reserved.
+        </span>
         <span className="text-sm text-gray-500">
-            © 2030 {" "}
-            <Link to="/" className="hover:underline text-black dark:text-white">Cinewave</Link>. All Rights Reserved.
+            <span className="font-medium text-gray-700 dark:text-gray-300"> Built by TABORA FRANCIS </span>
         </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-                <li> 
+                <li>
                     <NavLink 
                         to="https://codepen.io/Francis-tabora" target="blank_" rel="noreferrer" className="me-4 md:me-6">
                         Codepen
@@ -24,7 +28,7 @@ export const Footer = () => {
                 </li>
                 <li>
                     <NavLink 
-                        to="https://www.linkedin.com/in/francis-tabora-afata/" target="blank_" rel="noreferrer" className="me-4 md:me-6">
+                        to="https://www.linkedin.com/in/francis-tabora-afata/" target="_blank" rel="noreferrer" className="me-4 md:me-6">
                         LinkedIn
                     </NavLink>
                 </li>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { useDebounce } from "../hooks/useDebounce";
 import { Link, NavLink, useNavigate } from "react-router-dom";
-import Logo from "../assets/Default.png";
+import Logo from "../assets/favicon.ico";
 
 export const Header = () => {
   const [hidden, setHidden] = useState(true);
@@ -43,8 +43,8 @@ export const Header = () => {
       <nav className="bg-white border-b-2 border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900 dark:border-b-1 dark:border-gray-700">
         <div className="flex flex-wrap justify-between items-center mx-auto">
           <Link to="/" className="flex items-center">
-              <img src={Logo} className="mr-3 h-6 sm:h-9" alt="Cinemate Logo" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cinewave</span>
+              <img src={Logo} className="mr-3 h-6 sm:h-9" alt="Cinewave Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CineWave</span>
           </Link>
 
           <div id="mobile-nav" className="flex md:order-2">
